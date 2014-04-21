848a18a0e8941c735603fa3a1df4928fa50b5fca
Markus Schmall <schmall.markus@googlemail.com>
2014-04-21 21:37:11 +0200

    fixed downloading code

--- downloader.js
+++ downloader.js
@@ -5,15 +5,35 @@ var sys = require("sys"),
     fs = require("fs"),
     events = require("events");
 
+var exec = require('child_process').exec;
+
+
 var S = require('string');
 var db = require("./db");
+var config = require('/opt/nodepot/config');
 
 
 
+function finishCallBack(url, dest)
+{
+    console.log("Finished downloading: " + url + " to location " + dest);
+}
 
+
+function executer(command)
+{
+    var child = exec(command,
+        function (error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+}
 
 /*
- download export function
+ download export function executed only, if URL is not known yet
  url
  dest    target file name
  cb      call back
@@ -21,14 +41,24 @@ var db = require("./db");
 function downloadStep2(url, dest, cb)
 {
 
+    executer("curl -o " + dest + " " + url);
+
+/*
+
+    For now disabled
+
     var file = fs.createWriteStream(dest);
-    var request = http.get(url, function (response) {
+    var request = http.get(url, function (response)
+    {
         response.pipe(file);
         file.on('finish', function () {
             file.close();
-            cb();
+            cb(url, dest);
         });
     });
+
+*/
+
 }
 
 /*
@@ -39,8 +69,23 @@ function downloadStep2(url, dest, cb)
  */
 exports.download = function (url, dest, cb)
 {
-    url = S(url).toLowerCase(url);
-    db.isMemberMaliciousURL(url, dest, downloadStep2, cb);
+
+    var badInput = false;
+
+    if (S(dest).contains(".."))
+    {
+        badInput = true;
+    }
+
+    if (!badInput) {
+        url = S(url).toLowerCase(url);
+        db.isMemberMaliciousURL(url, dest, downloadStep2, cb);
+    }
+    else
+    {
+        console.log("Aborting download of " + url + " to location " + dest + " based on malicious user input");
+    }
 }
 
 exports.downloadStep2 = downloadStep2;
+exports.finishCallBack = finishCallBack;
