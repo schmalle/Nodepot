92cee86c846246cc7627e0b8970a9fd12c5df53e
Markus Schmall <schmall.markus@googlemail.com>
2014-04-21 21:35:56 +0200

    added config handling stuff

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 var server = require("./server");
 
 var runner = 0;
-var configFileName ="/etc/nodepot.cfg"
+var config = require('/opt/nodepot/config');
+var configFileName = "";
 
 
-/*
+console.log("Starting Nodepot in " + config.mode  + " mode ");
 
-    handle the command line input and expect default values where needed
-
- */
-
-process.argv.forEach(function (val, index, array)
-{
-    console.log(index + ': ' + val);
-
-    if (runner == 2)
-    {
-        configFileName = val;
-    }
-
-    runner++;
-
-});
-
-server.start(8888, configFileName);
+server.start();
 
 
