3ce2aea3b0ee5b633d688ade85333c4b664e6a0c
Markus Schmall <schmall.markus@googlemail.com>
2014-05-22 21:39:11 +0200

    removed obsolete code

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var server = require("./server");
 
-var runner = 0;
 var config = require('/opt/nodepot/config');
-var configFileName = "";
 
 
 console.log("Starting Nodepot in " + config.mode  + " mode ");
