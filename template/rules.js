47dd62b9da560caad4243a93992e36ebae4fae0b
Markus Schmall <schmall.markus@googlemail.com>
2014-05-25 20:52:09 +0200

    added rules

--- template/rules.js
+++ template/rules.js
@@ -2,7 +2,8 @@
 var attackStrings = ["-d+allow_url_include=on+-d+safe_mode=off+-d+suhosin.simulation=on+-d+disable_functions",
                      "WebAttack",
                      "option=com_jce&task=plugin&plugin=imgmanager&file=imgmanager", "Joomla attack",
-                     "/skin_shop/standard/3_plugin_twindow/twindow_notice.php?shop_this_skin_path", "Technote 7 RFI"];
+                     "/skin_shop/standard/3_plugin_twindow/twindow_notice.php?shop_this_skin_path", "Technote 7 RFI",
+                     "/wp-content/themes/striking/includes/timthumb.php?src=", "WP RFI"];
 
 
 exports.attackStrings = attackStrings;
