550b0c779981b579212a8b975be940e20ab9e063
Markus Schmall <schmall.markus@googlemail.com>
2014-05-27 07:27:12 +0200

    added more RFI

--- template/rules.js
+++ template/rules.js
@@ -3,7 +3,8 @@ var attackStrings = ["-d+allow_url_include=on+-d+safe_mode=off+-d+suhosin.simula
                      "WebAttack",
                      "option=com_jce&task=plugin&plugin=imgmanager&file=imgmanager", "Joomla attack",
                      "/skin_shop/standard/3_plugin_twindow/twindow_notice.php?shop_this_skin_path", "Technote 7 RFI",
-                     "/wp-content/themes/striking/includes/timthumb.php?src=", "WP RFI"];
+                     "/wp-content/themes/striking/includes/timthumb.php?src=", "WP RFI",
+                     "CONFIG[MWCHAT_Libs]=http://", "MWChat RFI"];
 
 
 exports.attackStrings = attackStrings;
